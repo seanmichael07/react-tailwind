1d29498b6b903d249fee54099668c94763f36c5e
Sean Burrows <seanmichael07@hotmail.com>
2020-03-02 14:21:15 +1000

    Initial project files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className="bg-gray-900 w-full h-screen p-3">
+      <div className="shadow container rounded mx-auto bg-white h-96 hover:bg-red-500 cursor-pointer px-10 py-5">
+        <p className="text-2xl text-gray-800">Hello there</p>
+        <p className="text-sm text-gray-600">Smaller font</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
